b52524e6273553eece20e994655fbea0a554ade3
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-01-22 02:04:50 +0100

    Interpret floats

--- interp.ml
+++ interp.ml
@@ -1,16 +1,37 @@
 open Ast
 
+type value =
+    | IntVal of int
+    | FloatVal of float
+;;
+
 let rec eval = function
-    | Int x -> x
-    | Float _ -> failwith "floats not implemented"
+    | Int x -> IntVal x
+    | Float x -> FloatVal x
+    | Var _ -> failwith "var not implemented"
+    | Let _ -> failwith "let not implemented"
     | BinOp (op, left, right) ->
             let left = eval left in
             let right = eval right in
             (match op with
-             | Add -> left + right
-             | Sub -> left - right
-             | Mul -> left * right
-             | Div -> left / right)
-    | Var _ -> failwith "var not implemented"
-    | Let _ -> failwith "let not implemented"
+             | Add -> eval_binop ( + ) ( +. ) left right
+             | Sub -> eval_binop ( - ) ( -. ) left right
+             | Mul -> eval_binop ( * ) ( *. ) left right 
+             | Div -> eval_binop ( / ) ( /. ) left right)
+
+
+and eval_binop op_int op_float left right =
+    match (left, right) with
+    | IntVal l, IntVal r -> IntVal (op_int l r)
+    | IntVal l, FloatVal r -> FloatVal (op_float (float_of_int l) r)
+    | FloatVal l, IntVal r -> FloatVal (op_float l (float_of_int r))
+    | FloatVal l, FloatVal r -> FloatVal (op_float l r)
 ;;
+
+let interpret expr =
+    let value = eval expr in
+    match value with
+    | IntVal n -> Printf.sprintf "%i" n
+    | FloatVal n -> Printf.sprintf "%f" n
+;;
+
--- main.ml
+++ main.ml
@@ -17,10 +17,28 @@ let test_inputs (list: string list) =
     List.iter (fun x -> test x) list 
 ;;
 
+let interpret input =
+    Printf.printf "Input: %S\n" input;
+    try
+        let result = Interp.interpret @@ parse input in
+        print_endline result
+    with e ->
+        Printf.printf "Error: %s\n\n" (Printexc.to_string e)
+;;
+
+let interpret_inputs (list: string list) =
+    List.iter (fun x -> interpret x) list
+;;
+
 let () = 
     (* test_inputs ["3.14"; ".5"; "3."; "."]; *)
     test_inputs ["let x = 5 in let y = 3 in x + y * 3"];
-    let calculation = "4 + 2 * 3 - 6 / 3 + 4" in
-    let result = Interp.eval @@ parse calculation in
-    Printf.printf "%i" result
+
+    let calculations = [
+        "4 + 2 * 3 - 6 / 3 + 4";
+        "2.5 + 2 * 3";
+        "2 + 2 * 3";
+        "(1 / 1000.0) * 500";
+    ] in 
+    interpret_inputs calculations
 ;;

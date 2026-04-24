4ff5bda586f6866fd05a15abf78fe36472b12dec
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-04-24 04:13:29 +0200

    Bulk run all code examples

--- src/main.ml
+++ src/main.ml
@@ -1,17 +1,26 @@
 
 let parse (filename: string) : Ast.expr =
-    let i_channel = open_in filename in
-    let lexbuf = Lexing.from_channel i_channel in
-    Parser.main Lexer.token lexbuf
+  let i_channel = open_in filename in
+  let lexbuf = Lexing.from_channel i_channel in
+  Parser.main Lexer.token lexbuf
 ;;
 
 let interpret (filename: string) =
-    try
-        let ast = parse filename in
-        let result = Interp.interpret ast in
-        print_endline result
-    with e ->
-        Printf.printf "Error: %s\n" (Printexc.to_string e)
+  try
+    let ast = parse filename in
+    let result = Interp.interpret ast in
+    print_endline filename;
+        print_endline ("=> " ^ result)
+  with e ->
+    Printf.printf "Error: %s\n" (Printexc.to_string e)
 ;;
 
-let () = interpret "code.example";;
+let files directory = 
+  Sys.readdir directory 
+    |> Array.to_list
+    |> List.map (fun name -> directory ^ "/" ^ name)
+
+
+let () = 
+  List.iter (fun file -> interpret file) (files "examples")
+;;
